d2b66f98f0a2ac8c03d0c43f05e89da84c06f4e1
hanaparker <hanaparker@icloud.com>
2023-02-03 16:41:12 -0600

    click on image to switch between two different cats

---
+++ meow.js
@@ -0,0 +1,22 @@
+var cat = [{'src':'./img/strawberryCat.jpg', 'length':"500", 'width':"430"},
+        {'src':'./img/cat.jpeg','length':"460",'width':"420"}]
+//anonymous function: ()=> or function()
+$(document).ready(()=>{
+    $("#meow").html("<b>this is a meow</b>");
+    $("#imagePlaceholder").on("click",function(){
+        switch (this.src.substring(this.src.lastIndexOf('/')+1)){
+            case "strawberryCat.jpg":
+                this.src=cat[1].src;
+                this.width=cat[1].width;
+                this.height=cat[1].length;
+                break;
+            default:
+                this.src=cat[0].src;
+                this.width=cat[0].width;
+                this.height=cat[0].length;
+       
+        }
+    });
+});
+
+
